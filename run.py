bd5b5924c07d8f64d96a98c806c68442bdff6c86
josswe26 <josec.sanchez@icloud.com>
2021-11-07 21:09:01 +0000

    Add functions to diplay welcome message and run all other functions of the program

--- run.py
+++ run.py
@@ -1,3 +1,39 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+def welcome_message():
+    '''
+    Display the welcome message.
+    '''
+    print('''
+    #     #                                                           
+    #  #  # ###### #       ####   ####  #    # ######    #####  ####  
+    #  #  # #      #      #    # #    # ##  ## #           #   #    # 
+    #  #  # #####  #      #      #    # # ## # #####       #   #    # 
+    #  #  # #      #      #      #    # #    # #           #   #    # 
+    #  #  # #      #      #    # #    # #    # #           #   #    # 
+     ## ##  ###### ######  ####   ####  #    # ######      #    ####  
+                                                                      
+                   #     #                             
+                   ##   ##   ##    ####  #####   ####  
+                   # # # #  #  #  #    # #    # #    # 
+                   #  #  # #    # #      #    # #    # 
+                   #     # ###### #      #####  #    # 
+                   #     # #    # #    # #   #  #    # 
+                   #     # #    #  ####  #    #  ####  
+                                                       
+   #####                                                                
+  #     #   ##   #       ####  #    # #        ##   #####  ####  #####  
+  #        #  #  #      #    # #    # #       #  #    #   #    # #    # 
+  #       #    # #      #      #    # #      #    #   #   #    # #    # 
+  #       ###### #      #      #    # #      ######   #   #    # #####  
+  #     # #    # #      #    # #    # #      #    #   #   #    # #   #  
+   #####  #    # ######  ####   ####  ###### #    #   #    ####  #    #
+    ''')
+
+    print('\nUse this calculator to help you discover how much of each macronutrient you should eat every day to reach your goals.')
+
+def main():
+    '''
+    Run all the functions of the program.
+    '''
+    welcome_message()
+
+main()
