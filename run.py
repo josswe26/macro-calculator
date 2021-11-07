266051f7cd9160f175d5cdc0270c99552e877d8d
josswe26 <josec.sanchez@icloud.com>
2021-11-07 21:17:55 +0000

    Add funtion to collect user's name

--- run.py
+++ run.py
@@ -30,10 +30,22 @@ def welcome_message():
 
     print('\nUse this calculator to help you discover how much of each macronutrient you should eat every day to reach your goals.')
 
+
+def collect_name():
+    '''
+    Collect the user name to be able
+    to access it later in the program
+    '''
+    return input('\nTo proceed, please enter your name: \n')
+
+
 def main():
     '''
     Run all the functions of the program.
     '''
     welcome_message()
+    name = collect_name()
+    print(f'Your name is: {name}')
+
 
 main()
