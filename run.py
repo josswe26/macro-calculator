de0b74dc7de83b4b1f6aa36728de5aa7717199b0
josswe26 <josec.sanchez@icloud.com>
2021-11-08 20:29:10 +0000

    Add function to handle the unit selection and add display message to the collect_name function

--- run.py
+++ run.py
@@ -36,7 +36,30 @@ def collect_name():
     Collect the user name to be able
     to access it later in the program
     '''
-    return input('\nTo proceed, please enter your name: \n')
+    name = input('\nTo proceed, please enter your name: \n')
+    print(f'\nThank you, {name.capitalize()}!')
+
+    return name
+
+
+def select_unit():
+    '''
+    Allow the user to make a selection of the desired
+    system of measurement to be used in the program
+    '''
+    unit = input('\nPlease select the system of measurement you would like to use.\n'
+    '\nIf you would like to use the Metric system, please enter 1.\n'
+    'If you would like to use the Imperial system, please enter 2.\n')
+    
+    if unit == '1':
+        print('\nGreat, you have selected the Metric system.')
+        return 1
+    elif unit == '2':
+        print('\nGreat, you have selected the Imperial system.')
+        return 2
+    else:
+        print('\nInvalid selection. You need to enter 1 or 2 to select the desired unit.')
+        select_unit()
 
 
 def main():
@@ -45,7 +68,7 @@ def main():
     '''
     welcome_message()
     name = collect_name()
-    print(f'Your name is: {name}')
+    unit = select_unit()
 
 
 main()
